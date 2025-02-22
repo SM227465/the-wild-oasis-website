2ab163e6e3917355941a0992d2dee49ba9e62fd7
Sourav Mandal <sm227465@gmail.com>
2025-02-22 19:46:07 +0530

    filter using query params

--- app/_components/CabinList.tsx
+++ app/_components/CabinList.tsx
@@ -1,9 +1,13 @@
-import { unstable_noStore as noStore } from 'next/cache';
 import { ICabin } from '../_interfaces/cabin';
 import { getCabins } from '../_lib/data-service';
 import CabinCard from './CabinCard';
 
-const CabinList = async () => {
+interface Props {
+  filter: string;
+}
+
+const CabinList = async (props: Props) => {
+  const { filter } = props;
   // noStore();
 
   const cabins: ICabin[] = await getCabins();
@@ -12,9 +16,29 @@ const CabinList = async () => {
     return null;
   }
 
+  let filtredCabins;
+
+  if (filter === 'all') {
+    filtredCabins = cabins;
+  }
+
+  if (filter === 'small') {
+    filtredCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  }
+
+  if (filter === 'medium') {
+    filtredCabins = cabins.filter(
+      (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+    );
+  }
+
+  if (filter === 'large') {
+    filtredCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+  }
+
   return (
     <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14'>
-      {cabins.map((cabin) => (
+      {filtredCabins?.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,11 +1,19 @@
 import { Suspense } from 'react';
 import CabinList from '../_components/CabinList';
 import Spinner from '../_components/Spinner';
+import Filter from '../_components/Filter';
 
 export const revalidate = 0;
 export const metadata = { title: 'Cabins' };
 
-const Page = () => {
+interface Props {
+  searchParams: { [key: string]: string | undefined };
+}
+
+const Page = (props: Props) => {
+  const { searchParams } = props;
+  const filter = searchParams?.capacity ?? 'all';
+
   return (
     <div>
       <h1 className='text-4xl mb-5 text-accent-400 font-medium'>
@@ -20,8 +28,12 @@ const Page = () => {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className='flex justify-end mb-8'>
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
