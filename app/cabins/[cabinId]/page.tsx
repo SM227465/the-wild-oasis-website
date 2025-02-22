c62f53722255a58ea0d72ec51ad65e1b96c6b33d
Sourav Mandal <sm227465@gmail.com>
2025-02-22 18:42:52 +0530

    few changes

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -1,3 +1,4 @@
+import TextExpander from '@/app/_components/TextExpander';
 import { ICabin } from '../../_interfaces/cabin';
 import { getCabin, getCabins } from '../../_lib/data-service';
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
@@ -52,7 +53,9 @@ const Page = async (props: Props) => {
             Cabin {name}
           </h3>
 
-          <p className='text-lg text-primary-300 mb-10'>{description}</p>
+          <p className='text-lg text-primary-300 mb-10'>
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className='flex flex-col gap-4 mb-7'>
             <li className='flex gap-3 items-center'>
