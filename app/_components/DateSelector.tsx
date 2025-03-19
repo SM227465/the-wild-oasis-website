a92276736ced47109cb8309a8eade95dbd15c1b1
Sourav Mandal <sm227465@gmail.com>
2025-03-19 21:03:22 +0530

    few changes

--- app/_components/DateSelector.tsx
+++ app/_components/DateSelector.tsx
@@ -1,42 +1,46 @@
 'use client';
 
-import { isWithinInterval } from 'date-fns';
+import { differenceInDays, isPast, isSameDay, isWithinInterval } from 'date-fns';
 import { type DateRange, DayPicker } from 'react-day-picker';
-import { ISetting } from '../_interfaces/setting';
-import { ICabin } from '../_interfaces/cabin';
-import { useState } from 'react';
 import 'react-day-picker/dist/style.css';
+import { ICabin } from '../_interfaces/cabin';
+import { ISetting } from '../_interfaces/setting';
 import { useReservation } from './ReservationContext';
 
-const isAlreadyBooked = (range: any, datesArr: any) => {
-  return (
-    range.from &&
-    range.to &&
-    datesArr.some((date: any) =>
-      isWithinInterval(date, { start: range.from, end: range.to })
-    )
+const isAlreadyBooked = (range: DateRange, datesArr: Date[]): boolean => {
+  if (!range.from || !range.to) {
+    return false; // Return false if either is undefined
+  }
+
+  return datesArr.some((date: Date) =>
+    isWithinInterval(date, { start: range.from!, end: range.to! })
   );
 };
 
 interface Props {
   settings: ISetting;
-  bookedDates: any;
+  bookedDates: Date[];
   cabin: ICabin;
 }
 
 const DateSelector = (props: Props) => {
   const { bookedDates, cabin, settings } = props;
   const { range, setRange, resetRange } = useReservation();
-  // CHANGE
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
-  // const range = { from: null, to: null };
+  const { discount, regularPrice } = cabin;
+  const { maxBookingLength, minBookingLength } = settings;
 
-  // SETTINGS
-  const minBookingLength = 1;
-  const maxBookingLength = 23;
+  let numberOfNights: number | null = null;
+  let cabinPrice: number | null = null;
+  let displayRange: DateRange | null = null;
+
+  if (range) {
+    displayRange = isAlreadyBooked(range, bookedDates) ? null : range;
+
+    if (displayRange && displayRange.to && displayRange.from) {
+      numberOfNights = differenceInDays(displayRange.to, displayRange.from);
+      cabinPrice = numberOfNights * (regularPrice - discount);
+    }
+  }
 
   return (
     <div className='flex flex-col justify-between'>
@@ -44,7 +48,7 @@ const DateSelector = (props: Props) => {
         className='pt-12 place-self-center'
         mode='range'
         onSelect={setRange}
-        selected={range}
+        selected={displayRange || undefined}
         min={minBookingLength + 1}
         max={maxBookingLength}
         fromMonth={new Date()}
@@ -52,6 +56,9 @@ const DateSelector = (props: Props) => {
         toYear={new Date().getFullYear() + 5}
         captionLayout='dropdown'
         numberOfMonths={2}
+        disabled={(currentDate) =>
+          isPast(currentDate) || bookedDates?.some((date) => isSameDay(date, currentDate))
+        }
       />
 
       <div className='flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]'>
@@ -60,19 +67,17 @@ const DateSelector = (props: Props) => {
             {discount > 0 ? (
               <>
                 <span className='text-2xl'>${regularPrice - discount}</span>
-                <span className='line-through font-semibold text-primary-700'>
-                  ${regularPrice}
-                </span>
+                <span className='line-through font-semibold text-primary-700'>${regularPrice}</span>
               </>
             ) : (
               <span className='text-2xl'>${regularPrice}</span>
             )}
             <span className=''>/night</span>
           </p>
-          {numNights ? (
+          {numberOfNights ? (
             <>
               <p className='bg-accent-600 px-3 py-2 text-2xl'>
-                <span>&times;</span> <span>{numNights}</span>
+                <span>&times;</span> <span>{numberOfNights}</span>
               </p>
               <p>
                 <span className='text-lg font-bold uppercase'>Total</span>{' '}
